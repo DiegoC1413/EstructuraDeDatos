699e6ebae6f44a44ef4e4e130b509b3dfdba9c23
DiegoC1413 <79611166+DiegoC1413@users.noreply.github.com>
2021-09-28 12:12:07 -0500

    Add files via upload

---
+++ Tarea3/Mesa.py
@@ -0,0 +1,11 @@
+print("MESA DE RESTAURANTE ")
+Detalle = {
+    "MESA": 3,
+    "COMENSAL": 4,
+    "ENTRADA": "Ensalda verde",
+    "MEDIO": "crema de zanahoria",
+    "FUERTE" : "Filete",
+    "ADICIONALES" : "Filete Termenino medio, la ensalada sin ningun tipos semilla, "
+                    "Adereso Ranch"
+}
+print(Detalle)
